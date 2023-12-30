170cdf58846b398e112d454f5f821f95e44d2df7
pandisundaram2812 <pandi.sundaram@anywhere.co>
2023-12-30 16:29:55 +0530

    fc

---
+++ src/test/java/com/pandi/mavenCI/MavenCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pandi.mavenCI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
